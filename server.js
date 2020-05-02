25efdb2c29443bf58f7ffb88c94ec6c85d39567e
Bdzanko17 <bdzanko@gmail.com>
2020-05-02 12:08:48 +0200

    active users

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongodb = require('mongodb').MongoClient;
 const clientt = require('socket.io').listen(4000).sockets;
+var connectedUsers = 0;
 
 mongodb.connect('mongodb+srv://bdzanko1:benjo1234@cluster0-s4065.mongodb.net/test?retryWrites=true&w=majority',
 function(err, client){
@@ -10,13 +11,24 @@ console.log("MongoDB connected");
 db = client.db('mongochat');
 
 
-clientt.on('connection',function(socket){
-    let chat = db.collection('chats');
 
+clientt.on('connection',function(socket){
+    console.log("connected");
+    connectedUsers++;
+    socket.broadcast.emit('active',connectedUsers)
+    socket.emit('active',connectedUsers)
+
+
+    socket.on('disconnect', function () {
+        console.log('disconnected')
+        connectedUsers--;
+        socket.broadcast.emit("active",connectedUsers)
+    });
+
+    let chat = db.collection('chats');
     sendStatus= function(s){
         socket.emit('status',s);
     }
-    //get chats from mongo collection
     chat.find().limit(100).sort({_id:1}).toArray(function(err,res) {
       if(err){
           throw err;
@@ -24,8 +36,11 @@ clientt.on('connection',function(socket){
       console.log(res);
       socket.emit('output',res)
     });
+
+
     
     socket.on('input',function(data) {
+        console.log(connectedUsers);
         let name = data.name;
         let message = data.message;
 
@@ -44,7 +59,6 @@ clientt.on('connection',function(socket){
         }
     });
 
-    //hanlde clear
     socket.on('clear',function(data){
         //remove all chats from collection
         chat.remove({},function(){
